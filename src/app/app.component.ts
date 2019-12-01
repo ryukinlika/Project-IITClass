96d6f14e1cdfa47962574b5aa4e0123613ea76a9
Michael Chen <michaelchen27@users.noreply.github.com>
2019-12-01 14:51:03 +0700

    Tambahin Tabs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
   title = 'PTI-Project';
 }
+
+
 
 
