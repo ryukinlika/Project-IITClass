f19f600223888caf42d64860153d1849f7d0aba6
William Lim <williaml1601@gmail.com>
2019-12-16 23:38:35 +0700

    trash

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,34 +1,34 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild } from "@angular/core";
 import {
   style,
   transition,
   animate,
   trigger,
   state
-} from '@angular/animations';
-import { UKM } from '../_shared/models/ukm';
-import { PelayananAPIService } from '../_shared/services/pelayanan-api.service';
-import { AuthAPIService } from '.././_shared/services/auth-api.service';
+} from "@angular/animations";
+import { UKM } from "../_shared/models/ukm";
+import { PelayananAPIService } from "../_shared/services/pelayanan-api.service";
+import { AuthAPIService } from ".././_shared/services/auth-api.service";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss'],
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.scss"],
   animations: [
-    trigger('fadeInOut', [
+    trigger("fadeInOut", [
       state(
-        'void',
+        "void",
         style({
           opacity: 0
         })
       ),
-      transition('void => *', animate(1000))
+      transition("void => *", animate(1000))
     ])
   ]
 })
 export class HomeComponent implements OnInit {
   private ukm: UKM = null;
-  private expires_at = '';
+  private expires_at = "";
   Arr: any;
   num = 5;
   i = 0;
@@ -37,7 +37,7 @@ export class HomeComponent implements OnInit {
   index: any;
   temp: any;
   numbercheck: number;
-  newArr: any[] = []
+  newArr: any[] = [];
   counter = 0;
   isFavorited = false;
   SS = 1;
@@ -47,24 +47,8 @@ export class HomeComponent implements OnInit {
   constructor(
     private pelayanApi: PelayananAPIService,
     private authAPI: AuthAPIService
-  ) { }
+  ) {}
 
-  findFave(data: any) {
-    if (this.favo.findIndex(result => result.kode == data.kode) > -1) {
-      return {
-        fas: true,
-        'fa-heart': true,
-        'icon-hati-full': true,
-        'fa-lg': true
-      };
-    }
-    return {
-      far: true,
-      'fa-heart': true,
-      'icon-hati': true,
-      'fa-lg': true
-    };
-  }
   ngOnInit() {
     this.pelayanApi.getAllUKM().subscribe(
       result => {
@@ -72,19 +56,23 @@ export class HomeComponent implements OnInit {
         this.sortby("kode");
         this.Arr = this.ukm.result.ukm;
 
-
         //Check berapa banyak setiap kode jumlahnya, dan filter yang idnya tidak benar
         for (let i = 0; i < this.Arr.length; i++) {
-          this.temp = (this.Arr[i].kode).substring(2, 5)
+          this.temp = this.Arr[i].kode.substring(2, 5);
           this.numbercheck = parseInt(this.temp);
           if (this.Arr[i].kode.includes("SB") && this.numbercheck == this.SB) {
             this.SB += 1;
-          } else if (this.Arr[i].kode.includes("SS") && this.numbercheck == this.SS) {
+          } else if (
+            this.Arr[i].kode.includes("SS") &&
+            this.numbercheck == this.SS
+          ) {
             this.SS += 1;
-          } else if (this.Arr[i].kode.includes("OL") && this.numbercheck == this.OL) {
+          } else if (
+            this.Arr[i].kode.includes("OL") &&
+            this.numbercheck == this.OL
+          ) {
             this.OL += 1;
-          }
-          else {
+          } else {
             continue;
           }
         }
@@ -108,7 +96,7 @@ export class HomeComponent implements OnInit {
   }
 
   sortby(str: string) {
-    if (str == 'kode') {
+    if (str == "kode") {
       this.ukm.result.ukm.sort((a: any, b: any) => {
         if (a.kode < b.kode) {
           return -1;
@@ -119,7 +107,7 @@ export class HomeComponent implements OnInit {
         }
       });
     }
-    if (str == 'nama') {
+    if (str == "nama") {
       this.ukm.result.ukm.sort((a: any, b: any) => {
         if (a.nama < b.nama) {
           return -1;
@@ -130,7 +118,7 @@ export class HomeComponent implements OnInit {
         }
       });
     }
-    if (str == 'anggota') {
+    if (str == "anggota") {
       this.ukm.result.ukm.sort((a: any, b: any) => {
         if (a.anggota < b.anggota) {
           return -1;
@@ -141,7 +129,7 @@ export class HomeComponent implements OnInit {
         }
       });
     }
-    if (str == 'updated_at') {
+    if (str == "updated_at") {
       this.ukm.result.ukm.sort((a: any, b: any) => {
         if (a.updated_at < b.updated_at) {
           return -1;
@@ -156,32 +144,47 @@ export class HomeComponent implements OnInit {
 
   addFave(data: any) {
     if (!this.isLoggedIn) {
-      alert('Silahkan login terlebih dahulu');
+      alert("Silahkan login terlebih dahulu");
       return;
     }
     if (
-      localStorage.getItem('favourited' + localStorage.getItem('user_name')) !=
+      localStorage.getItem("favourited" + localStorage.getItem("user_name")) !=
       null
     )
       this.favo = JSON.parse(
-        localStorage.getItem('favourited' + localStorage.getItem('user_name'))
+        localStorage.getItem("favourited" + localStorage.getItem("user_name"))
       );
     // console.log(this.favo);
 
     if (this.favo.findIndex(result => result.kode == data.kode) == -1) {
       this.favo.push(data);
       localStorage.setItem(
-        'favourited' + localStorage.getItem('user_name'),
+        "favourited" + localStorage.getItem("user_name"),
         JSON.stringify(this.favo)
       );
       this.isFavorited = true;
-      alert('Berhasil menambahkan ke favorit!');
+      alert("Berhasil menambahkan ke favorit!");
       return;
     }
 
-    alert('Tidak dapat menambahkan UKM yang sama!');
+    alert("Tidak dapat menambahkan UKM yang sama!");
     return;
   }
 
-
+  findFave(data: any) {
+    if (this.favo.findIndex(result => result.kode == data.kode) > -1) {
+      return {
+        fas: true,
+        "fa-heart": true,
+        "icon-hati-full": true,
+        "fa-lg": true
+      };
+    }
+    return {
+      far: true,
+      "fa-heart": true,
+      "icon-hati": true,
+      "fa-lg": true
+    };
+  }
 }
